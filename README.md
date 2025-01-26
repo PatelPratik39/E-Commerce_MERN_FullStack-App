9c3a0c5ca1f9c6d1cde4c24f319efd086f20b5b7
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-26 13:50:25 -0600

    payment COntroller

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 # upStash for redis :https://console.upstash.com/
 
+# Strip for payment : https://dashboard.stripe.com/test/dashboard
+
 ![image](./redis_tokens.png)
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentsRoutes from "./routes/payments.route.js"
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -21,7 +22,8 @@ const PORT = process.env.PORT || 3001;
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
-app.use("/api/coupens", couponRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is up and Running on http://localhost:${PORT}`);
