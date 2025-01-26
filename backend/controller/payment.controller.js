580ddf2c8bc9eb30cba86ecb08d954039ff9d82e
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-26 15:24:37 -0600

    Payment Service configured

--- backend/controller/payment.controller.js
+++ backend/controller/payment.controller.js
@@ -1,5 +1,6 @@
-import Coupon from "../models/coupon.model.js";
 import { stripe } from "../lib/stripe.js";
+import Coupon from "../models/coupon.model.js";
+import Order from "../models/order.model.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -53,9 +54,17 @@ export const createCheckoutSession = async (req, res) => {
             }
           ]
         : [],
+      // metadata
       metadata: {
         userId: req.user._id.toString(),
-        couponCode: couponCode || " "
+        couponCode: couponCode || " ",
+        products: JSON.stringify(
+          products.map((p) => ({
+            id: p._id,
+            quantity: p.quantity,
+            price: p.price
+          }))
+        )
       }
     });
 
@@ -64,8 +73,56 @@ export const createCheckoutSession = async (req, res) => {
     }
     res.status(200).json({ id: session.id, totalAmount: totalAmount / 100 });
   } catch (error) {
-    console.log("Error in createCheckoutSession controller", error.message);
-    res.status(500).json({ message: "Server error", error: error.message });
+    console.log("Error processing checkout :", error);
+    res.status(500).json({
+      message: "Error processing successful checkout",
+      error: error.message
+    });
+  }
+};
+
+export const checkoutSuccess = async (req, res) => {
+  try {
+    const { sessionId } = req.body;
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+    if (session.payment_status === "paid") {
+      if (session.metadata.couponCode) {
+        await Coupon.findOneAndUpdate(
+          {
+            code: session.metadata.couponCode,
+            userId: session.metadata.userId
+          },
+          {
+            isActive: false
+          }
+        );
+      }
+      //   create a new order
+      const products = JSON.parse(session.metadata.products);
+      const newOrder = new Order({
+        user: session.metadata.userId,
+        products: products.map((product) => ({
+          product: product.id,
+          quantity: product.quantity,
+          price: product.price
+        })),
+        totalAmout: session.amount_total / 100,
+        stripSessionId: sessionId
+      });
+      await newOrder.save();
+      res.status(200).json({
+        success: true,
+        message:
+          "Payment successful, Order created, and coupon deactivated if used.",
+        orderId: newOrder._id
+      });
+    }
+  } catch (error) {
+    console.log("Error processing successful checkout : ", error);
+    res.status(500).json({
+      message: "Error processing successful checkout",
+      error: error.message
+    });
   }
 };
 
