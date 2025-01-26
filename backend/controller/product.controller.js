f4daa12e8db433c81432bcc5957f352f8631b3eb
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-26 01:55:24 -0600

    productroute

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -6,6 +7,31 @@ export const getAllProducts = async (req, res) => {
     res.json({ products });
   } catch (error) {
     console.error("Error getting All Products", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("festured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+    // if not in redis, fetch it from mongoDb Database
+    // .lean() returns plain javascript object insted of mongodb document
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res
+        .status(404)
+        .json({ message: "No Featured Products found!!! " });
+    }
+    // store in redis for feature quick access
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in getFeaturedProducts controller", error.message);
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
