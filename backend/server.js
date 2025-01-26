91fca9df7900843e14f2b4ae459a109d2283a155
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-25 19:45:56 -0600

    basic routes

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js"
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Authentication
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is up and Running on http://localhost:${PORT}`);
+});
