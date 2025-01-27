9bc5fb64ff0dba6c9e6792a67274969a4c35862f
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-26 19:42:52 -0600

    analytics route and server file

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
-import paymentsRoutes from "./routes/payments.route.js"
-import analyticsRoutes from "./routes/analytics.route.js"
+import paymentsRoutes from "./routes/payments.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
