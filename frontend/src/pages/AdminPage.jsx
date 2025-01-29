987ff874bfd84c2d46f084b105eb237253db9fc0
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-28 19:37:46 -0600

    products renders in admin Dashboiard and can able to delete based on admin role

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,10 +1,10 @@
 import { BarChart, PlusCircle, ShoppingBasket } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import AnalyticsTab from "../components/AnalyticsTab";
 import ProductList from "../components/ProductList";
 import CreateProductForm from "../components/CreateProductForm";
-
+import { useProductStore } from "../stores/useProductStore";
 
 const tabs = [
   { id: "create", label: "Create Product", icon: PlusCircle },
@@ -13,6 +13,12 @@ const tabs = [
 ];
 const AdminPage = () => {
   const [activeTab, setActiveTab] = useState("create");
+  const { fetchAllProducts } = useProductStore();
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, [fetchAllProducts]);
+
   return (
     <>
       <div className="min-h-screen relative overflow-hidden">
