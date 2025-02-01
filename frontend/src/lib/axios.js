5bc2f98f893cfb302c154ed2bc9d4a6a0c16b820
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-01 16:25:47 -0600

    url change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -28,7 +28,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://your-backend.onrender.com/api", // Explicit production backend URL
+      : "https://e-commerce-mern-fullstack-app.onrender.com/api", // Explicit production backend URL
   withCredentials: true // Allow cookies
 });
 
