a806578ff10dcc74cc16652297955ed6e5ee3cdf
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-01 14:52:11 -0600

    server.js file

--- backend/server.js
+++ backend/server.js
@@ -16,18 +16,26 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
-
 app.use(
   cors({
-    origin: "https://e-commerce-mern-fullstack-app.onrender.com", // ✅ Allow frontend
-    credentials: true, // ✅ Required for authentication cookies
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    origin: ["https://e-commerce-mern-fullstack-app.onrender.com"],
+    credentials: true
   })
 );
 
+
+// app.use(
+//   cors({
+//     origin: "https://e-commerce-mern-fullstack-app.onrender.com", // ✅ Allow frontend
+//     credentials: true, // ✅ Required for authentication cookies
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+//   })
+// );
+
 // app.use(cors());
 app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Authentication
