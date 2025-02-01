660d06cf760974b1b7a7d4799a372f70f0289113
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-01 14:16:24 -0600

    v

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+// import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cart.route.js";
@@ -17,14 +17,14 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173", // ✅ Allow frontend
-    credentials: true, // ✅ Required for authentication cookies
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173", // ✅ Allow frontend
+//     credentials: true, // ✅ Required for authentication cookies
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+//   })
+// );
 
 // app.use(cors());
 app.use(express.json({ limit: "20mb" }));
