12dc31fdc915eca9200e21a7f0b66fbd8f61f818
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-01 15:09:15 -0600

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-// import cors from "cors";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cart.route.js";
@@ -25,15 +25,25 @@ connectDB();
 //   })
 // );
 
-
+// localhost --- for development enviroment , 
 // app.use(
 //   cors({
-//     origin: "https://e-commerce-mern-fullstack-app.onrender.com", // ✅ Allow frontend
+//     origin: "http://localhost:5173", // ✅ Allow frontend
 //     credentials: true, // ✅ Required for authentication cookies
 //     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 //     allowedHeaders: ["Content-Type", "Authorization"]
 //   })
 // );
+
+// For Production :// ✅ Enable CORS
+app.use(
+  cors({
+    origin: "https://e-commerce-mern-fullstack-app.onrender.com",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
 // app.use(cors());
 app.use(express.json({ limit: "20mb" }));
@@ -51,6 +61,12 @@ app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  // ✅ Prevent API Calls from Being Redirected to React App
+  app.get("/api/*", (req, res) => {
+    res.status(404).json({ error: "API route not found" });
+  });
+  
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
